c9dab036e802309d66f8101fde0ce5cbc745c962
coderInPractice <coderInPractice@noreply.github.com>
2020-01-20 01:19:53 +0530

    Third Commit.

---
+++ Basic_Programmes/src/pattern/Inverse_Right_Triangle.java
@@ -0,0 +1,27 @@
+package pattern;
+
+import java.util.Scanner;
+
+public class Inverse_Right_Triangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter the number of rows:");
+		
+		int rows = input.nextInt();
+		
+		input.close();
+		
+		for(int i=1;i<=rows;i++) {
+			for(int j=rows;j>=i;j--) {
+				System.out.print(" * ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
