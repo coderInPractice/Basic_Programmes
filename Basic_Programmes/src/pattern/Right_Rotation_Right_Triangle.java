e71ce86242e075cea9d1c13e00c75d2c9bd9fff9
coderInPractice <coderInPractice@noreply.github.com>
2020-01-20 01:52:36 +0530

    Fourth Commit.

---
+++ Basic_Programmes/src/pattern/Right_Rotation_Right_Triangle.java
@@ -0,0 +1,30 @@
+package pattern;
+
+import java.util.Scanner;
+
+public class Right_Rotation_Right_Triangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter the nummber of rows:");
+		
+		int rows = input.nextInt();
+		
+		input.close();
+		
+		for(int i=1;i<=rows;i++) {
+			for(int j=rows-1;j>=i;j--) {
+				System.out.print(" ");
+			}
+			for(int k=1;k<=i;k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
